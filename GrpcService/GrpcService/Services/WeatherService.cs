37687c4ef70a2e2adc82495e7c23b65a8112e242
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-06-02 13:57:42 +0500

    added weather service and proto

---
+++ GrpcService/GrpcService/Services/WeatherService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace GrpcService.Services
+{
+    public class WeatherService : Weather.WeatherBase
+    {
+        public override Task<WeatherResponse> GetCurrentWeather(WeatherRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new WeatherResponse() {});
+        }
+    }
+}
