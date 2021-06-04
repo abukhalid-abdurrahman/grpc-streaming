a731289a998472a02c5025aebb79f937869da042
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-06-04 19:30:06 +0500

    chat service added

---
+++ GrpcService/GrpcService/Services/ChatService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcService.Services
+{
+    public class ChatService : Chat.ChatBase
+    {
+        private readonly ILogger<ChatService> _logger;
+
+        public ChatService(ILogger<ChatService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task SendMessage(
+            IAsyncStreamReader<RequestMessage> requestStream, 
+            IServerStreamWriter<ResponseMessage> responseStream, 
+            ServerCallContext context)
+        {
+            
+        }
+    }
+}
